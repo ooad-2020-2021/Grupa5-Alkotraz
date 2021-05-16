2cd8c5ed95e6854c13c064ae20ad1c6f0d45afa1
Danijel0 <dmiokovic1@etf.unsa.ba>
2021-05-17 00:26:58 +0200

    gdg

---
+++ Implementacija/WebApplication1/WebApplication1/Models/Placanje.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+	{
+	    public class Placanje
+    {
+		[Required]
+		[DataType(DataType.Date)]
+		public DateTime datumUplate { get; set; }
+		[Required]
+		[DataType(DataType.Time)]
+		public DateTime vrijemeUplate { get; set; }
+		[Required]
+		public double iznosUplate { get; set; }
+		public Placanje(DateTime datum, DateTime vrijeme, double iznos)
+        {
+			datumUplate = datum;
+			vrijemeUplate = vrijeme;
+			iznosUplate = iznos;
+        }
+	}
+		
+	}
