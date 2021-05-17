76a102916515ae72c30626432fb84126d63b17ab
Danijel0 <dmiokovic1@etf.unsa.ba>
2021-05-17 10:58:35 +0200

    dodana klasa Rezervacija

---
+++ Implementacija/WebApplication1/WebApplication1/Models/Rezervacija.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Rezervacija
+	{
+		[Required]
+		public Stol izabraniStol { get; set; }
+		[Required]
+		[DataType(DataType.Date)]
+		public DateTime datumRezervacije { get; set; }
+		[Required]
+		[DataType(DataType.Time)]
+		public DateTime vrijemeRezervacije { get; set; }
+		[Required]
+		public int kodRezervacije { get; set; }
+		public Rezervacija(Stol stol, DateTime datum, DateTime vrijeme)
+        {
+			izabraniStol = stol;
+			datumRezervacije = datum;
+			vrijemeRezervacije = vrijeme;
+        }
+	}
+}
