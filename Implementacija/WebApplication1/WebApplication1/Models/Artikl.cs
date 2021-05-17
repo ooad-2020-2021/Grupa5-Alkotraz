96268eb9adee68124614e18f698618b1a26f3bfa
Danijel0 <dmiokovic1@etf.unsa.ba>
2021-05-17 10:37:40 +0200

    dodana klasa Artikl

---
+++ Implementacija/WebApplication1/WebApplication1/Models/Artikl.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Artikl
+	{
+		[Required]
+		public String naziv { get; set; }
+		[Required]
+		public double cijena { get; set; }
+		[Required]
+		public int kolicina { get; set; }
+		public Artikl(String ime, double cijena)
+        {
+			naziv = ime;
+			this.cijena = cijena;
+        }
+		
+	}
+}
