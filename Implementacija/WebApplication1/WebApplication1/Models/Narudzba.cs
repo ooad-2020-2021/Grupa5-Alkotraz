98706260c58e383c023aa137514cf2b0379a0dea
Danijel0 <dmiokovic1@etf.unsa.ba>
2021-05-17 10:29:26 +0200

    dodana klasa Narudzba

---
+++ Implementacija/WebApplication1/WebApplication1/Models/Narudzba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+	public class Narudzba
+	{
+		[NotMapped]
+		public List<Artikl> artikli { get; set; }
+		[Required]
+		public double cijena { get; set; }
+		[Required]
+		public int brojNarudzbe { get; set; }
+		[Required]
+		public Status statusNarudzbe { get; set; }
+		[Required]
+		public Stol stol { get; set; }
+		[Required]
+		public Placanje placanje;
+		public Narudzba(List<Artikl> artikl);
+	}
+}
